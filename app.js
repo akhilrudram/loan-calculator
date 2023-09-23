1b9acefe93ac0e99b28357a2a9e899f0add6e310
akhilsamsidh <akhil.samsidh@gmail.com>
2023-09-23 17:32:03 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,66 @@
+//Listen For Submit
+document.getElementById('loan-form'),addEventListener('submit', function(e){
+    //hide results
+   document.getElementById('results').style.display = 'none';
+  //show loader
+  document.getElementById('loading').style.display = 'block';
+
+  this.setTimeout(calculateResults, 2000);
+
+   e.preventDefault();
+});
+
+function calculateResults(){
+   console.log('calculating..');
+
+   const amount = document.getElementById('amount');
+   const interest = document.getElementById('interest');
+   const years = document.getElementById('years');
+   const monthlyPayment = document.getElementById('monthly-payment');
+   const totalPayment = document.getElementById('total-payment');
+   const totalInterest = document.getElementById('total-interest');
+
+   const principal = parseFloat(amount.value);
+   const calculateInterest = parseFloat(interest.value)/100/12;
+   const calculatedPayments = parseFloat(years.value)*12;
+
+
+   const x = Math.pow(1 + calculateInterest, calculatedPayments);
+   const monthly = (principal*x*calculateInterest)/(x-1);
+
+   if(isFinite(monthly)){
+       monthlyPayment.value = monthly.toFixed(2);
+       totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+       totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+       document.getElementById('results').style.display = 'block';
+
+       document.getElementById('loading').style.display = 'none';
+
+   }else{
+      showError('Please Check Your Numbers');
+   }
+
+}
+
+function showError(error){
+
+   document.getElementById('results').style.display = 'none';
+
+       document.getElementById('loading').style.display = 'none';
+   const errorDiv = document.createElement('div');
+
+   const card = document.querySelector('.card');
+   const heading = document.querySelector('.heading');
+
+   errorDiv.className = 'alert alert-danger';
+
+   errorDiv.appendChild(document.createTextNode(error));
+
+   card.insertBefore(errorDiv, heading);
+
+   setTimeout(clearError, 3000);
+}
+function clearError(){
+   document.querySelector('.alert').remove();
+}
